0c1b07746d040a970c25e2335869ec150c0e7b10
Victor Volpi <112173184+VictorVolpi@users.noreply.github.com>
2022-11-08 18:06:00 -0300

    Loja de Roupa
    Lição bdLojadeRoupa com insert e update

--- Insert.sql
+++ Insert.sql
@@ -1,75 +1,76 @@
-USE bdEmpresa
+USE bdLojadeRoupa
 
-INSERT INTO tbDepartamento (nomeDepartamento)
+INSERT INTO tbCliente (nomeCliente, idadeCliente)
 VALUES
-	('Vendas')
-	,('Supervisor')
-	,('Tecnologia')
-	,('Finanças')
-	,('Advogada')
-	,('Diretoria')
-	,('Marketing Digital')
-	SELECT * FROM tbDepartamento
+	('Kaua de Souza',22)
+	,('Diego Olegario',22)
+	,('Ruthe Almeida',21)
+	,('Rosangela Freire',45)
+	,('Vanessa Lopes',43)
+	SELECT * FROM tbCliente
 
-INSERT INTO tbFuncionario (nomeFuncionario, cpfFuncionario, rgFuncionario, idDepartamento)
+INSERT INTO tbFabricante (nomeFabricante)
 VALUES
-	('Kaua de Souza', '505.596.356-69', '61.223.666-9', 1)
-	,('Victor Volpi',  '504.594.368-56', '60.220.664-9', 2)
-	,('Bruno Silva' , '540.595.365-65' , '62.224.656-9', 3)
-	,('Diego Olegario' , '541.596.366-65' , '63.225.657-9', 4)
-	,('Gabrielle Oliveira Santana', '542.597.367-66' , '64.226.658-9', 5)
-	,('Gabrielle Rocha Freitas' ,'543.598.368-65' , '68.228.658-9', 6)
-	,('Ruthe Almeida' ,'590.599.369-69' , '69.225.657-9', 7)
+	('Malwee')
+	,('Marisol')
+	,('Cia da Malha')
+	SELECT * FROM tbFabricante
+
+INSERT INTO tbVendedor (nomeVendedor) 
+VALUES
+
+	('João Santana')
+	,('Raquel Torres')
+	SELECT * FROM tbVendedor
+
+INSERT INTO tbFuncionario (nomeFuncionario, idadeFuncionario, dataAdimissao, salarioFuncionario)
+VALUES
+	('Victor Volpi', 21, '11/05/2022', 1200)
+	,('Bruno Silva' , 22, '11/05/2022', 1200)
 	SELECT * FROM tbFuncionario
 
-INSERT INTO tbFoneFuncionario (numFone, idFuncionario)
+INSERT INTO tbVenda (dataVenda , totalVenda  , idCliente, idVendedor) 
 VALUES
-	('+55(11)96275-4521', 1)
-	,('+55(11)98142-0861', 2)
-	,('+55(11)95386-0905', 3)
-	,('+55(11)97647-9278',4)
-	,('+55(11)98540-8603', 5)
-	,('+55(11)97202-4965', 6)
-	,('+55(11)95323-5148', 7)
-	SELECT * FROM tbFoneFuncionario
+		('15/08/2022',699.99,1,1)
+		,('17/09/2022',449.99,2,1)
+		,('19/10/2022',400,3,1)
+		,('21/11/2022',364.84,4,2)
+		,('23/12/2022',372.99,5,2)
+	
+	SELECT * FROM tbVenda
 
-INSERT INTO tbProjeto (nomeProjeto , cargaHoraria) 
-VALUES
-
-	('MetodoCurio',8)
-	,('MetodoCurio',8)
-	,('MetodoCurio',8)
-	,('MetodoCurio',8)
-	SELECT * FROM tbProjeto
-
-INSERT INTO tbFuncionarioProjeto (idFuncionario , idProjeto , qtdeHoras) 
+	INSERT INTO tbProduto (nomeProduto , preçoProduto  ,dataEntradaProduto, estoqueProduto, idFuncionario, idFabricante ) 
 VALUES
 	
-	(1, 1, 8 )
-	,(2, 2, 8 )
-	,(3, 3, 8 )
-	,(4, 4, 8)
-	SELECT * FROM tbFuncionarioProjeto
+	('Camisetas Nike',99.99,'02/07/2022',64,1,1)
+	,('Tênis Nike',150,'05/07/2022',88,2,1)
+	,('Blusa Nike',349,'08/07/2022',75,1,1)
+	,('Camisetas Adidas',59.99,'02/07/2022',64,2,2)
+	,('Tênis Adidas',120,'05/07/2022',88,1,2)
+	,('Blusa Adidas',300,'08/07/2022',75,2,2)
+	,('Camisetas Puma',99.99,'02/07/2022',64,1,3)
+	,('Tênis Puma',150,'05/07/2022',88,2,3)
+	,('Blusa Puma',349,'08/07/2022',75,1,3)
 
-INSERT INTO tbConjuge (nomeConjuge , idFuncionario)
+	SELECT * FROM tbProduto
+
+
+	INSERT INTO tbItensVenda(quantidadeItens, subTotalItens,idVenda,idProduto ) 
 VALUES
 
-	('Camila Loures',1)
-	,('Luara Fonseca',2)
-	,('Larissa Manoela',3)
-	,('Luísa Sonza',4)
-	,('Pedro Henrique Efigênio da rocha',5)
-	,('Thomaz Costa',6)
-	,('Lucas Santos',7)
-	SELECT * FROM tbConjuge
+		(15,64,1,1)
+		,(26,88,1,2)
+		,(5,75,2,3)
+		,(7,64,2,4)
+		,(8,75,3,6)
+		,(6,64,3,7)
+		,(4,88,4,5)
+		,(6,88,4,8)
+		,(2,75,5,9)
+		,(5,64,5,7)
 
-INSERT INTO tbDependente (nomeDependente, dtNasctoDependente, idFuncionario)
-VALUES
-		('Carla Loures de Souza','25/01/2016' , 1)
-		,('Gael Volpi Fonseca' , '26/03/2015' ,2)
-		,('Arthur Silva',  '27/03/2014',3)
-		,('Helena Sonza','12/09/2015' , 4)
-		,('Yeonimin Oliveira Rocha ','20/05/2017' ,5)
-		,('Alice Rocha','10/09/2017', 6)
-		,('Laura Santos de Almeida', '19/06/2016',7)
-		SELECT * FROM tbDependente
+
+	SELECT * FROM tbItensVenda
+
+
+
